972eb24af3dfd98ef17c2a1a1213ffa1f6f63a5b
Mario Lasić <mario.lasic95@gmail.com>
2020-06-30 20:40:23 +0200

    Added object Earthquakes

--- app/src/main/java/Earthquakes.java
+++ app/src/main/java/Earthquakes.java
@@ -1,2 +1,37 @@
 public class Earthquakes {
+
+    private String mPlace;
+    private String mDate;
+    private String mPower;
+
+    // Constructor
+    public Earthquakes(String place, String date, String power) {
+        mPlace = place;
+        mDate = date;
+        mPower = power;
+    }
+    //Getters and Setters for constructor
+    public String getPlace() {
+        return mPlace;
+    }
+
+    public void setPlace(String place) {
+        mPlace = place;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+    public void setDate(String date) {
+        mDate = date;
+    }
+
+    public String getPower() {
+        return mPower;
+    }
+
+    public void setPower(String power) {
+        mPower = power;
+    }
 }
